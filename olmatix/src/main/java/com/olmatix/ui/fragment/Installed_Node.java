f0bf6856bf0e83d763a7923f3678e9151ff69335
Lesjaw <lesjaw@gmail.com>
2016-12-11 23:22:15 +0700

    remove database handle, now database will handle by service

--- olmatix/src/main/java/com/olmatix/ui/fragment/Installed_Node.java
+++ olmatix/src/main/java/com/olmatix/ui/fragment/Installed_Node.java
@@ -4,11 +4,7 @@ package com.olmatix.ui.fragment;
  * Created by Lesjaw on 05/12/2016.
  */
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -19,20 +15,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.olmatix.adapter.OlmatixAdapter;
 import com.olmatix.database.dbNodeRepo;
@@ -40,13 +33,11 @@ import com.olmatix.helper.OnStartDragListener;
 import com.olmatix.helper.SimpleItemTouchHelperCallback;
 import com.olmatix.lesjaw.olmatix.R;
 import com.olmatix.model.Installed_NodeModel;
-import com.olmatix.service.OlmatixService;
 import com.olmatix.utils.Connection;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.w3c.dom.Node;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -72,11 +63,8 @@ public class Installed_Node extends Fragment implements OnStartDragListener {
     public static dbNodeRepo dbNodeRepo;
     private Installed_NodeModel installedNodeModel;
     private String inputResult;
-    private String NodeID;
-    private  String mMessage;
-    private String NodeSplit;
-    int flag =0;
-    int flagNodeAdd =0;
+
+
 
     @Nullable
     @Override
@@ -103,178 +91,43 @@ public class Installed_Node extends Fragment implements OnStartDragListener {
         mFab.setOnClickListener(mFabClickListener());
     }
 
-    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            String device = intent.getStringExtra("MQTT devices");
-            String message = intent.getStringExtra("MQTT message");
-            Log.d("receiver", " = " + device + " : "+ message);
-            NodeSplit = device;
-            String[] outputDevices = NodeSplit.split("/");
-            NodeID = outputDevices[1];
-            mMessage = message;
-            device = device.substring(device.indexOf("$")+1,device.length());
-            messageReceive.put(device,message);
-            if (flagNodeAdd==1) {
-                addCheckValidation();
-            }
-            if(flagNodeAdd == 0 ) {
-                saveandpersist();
-            }
-        }
-
-    };
-
-    private void addCheckValidation(){
-        if(messageReceive.containsKey("online")){
-            Log.d("addCheckValid 1", "Passed");
-            if (inputResult.equals(NodeID)){
-                Log.d("addCheckValid 2", "Passed");
-                if (mMessage.equals("true")){
-                    Log.d("addCheckValid 3", "Passed");
-                    Log.d("Running = ", "saveIfOnline");
-                    saveIfOnline();
-
-                }
-            }
-        }
-
-    }
-
-    private void saveIfOnline() {
-
-        if (messageReceive.containsKey("online")) {
-
-            for (int i = 0; i < dbNodeRepo.getNodeList().size(); i++) {
-                if (data.get(i).getNodesID().equals(NodeID)) {
-                    Toast.makeText(getActivity(), "You already have this Node ID", Toast.LENGTH_LONG).show();
-                    flag = 1;
-                    Log.d("saveIfOnline", "You already have this Node, DB = " + i + " flag = " + flag);
-                }
-            }
-
-            if (flag != 1) {
-                installedNodeModel.setNodesID(NodeID);
-                installedNodeModel.setOnline(messageReceive.get("online"));
-
-
-                dbNodeRepo.insertDb(installedNodeModel);
-                flagNodeAdd = 0;
-                Toast.makeText(getActivity(), "Add Node Successfully", Toast.LENGTH_LONG).show();
-                Log.d("saveIfOnline", "Add Node success, " + " flag = " + flag);
-                messageReceive.clear();
-                doSubcribeIfOnline();
-
-            }
-
-        }
-
-    }
-
-    private void doSubcribeIfOnline(){
-        String topic = "devices/" + inputResult + "/#";
-        int qos = 1;
-        try {
-            IMqttToken subToken = Connection.getClient().subscribe(topic, qos);
-            subToken.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    messageReceive.put("NodeId",inputResult);
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void saveandpersist() {
-
-
-        installedNodeModel.setNodesID(NodeID);
-
-        if (messageReceive.get("nodes") != null) {
-            installedNodeModel.setNodes(messageReceive.get("nodes"));
-        }
-        if (messageReceive.get("name") != null) {
-            installedNodeModel.setName(messageReceive.get("name"));
-        }
-        if (messageReceive.get("localip") != null) {
-            installedNodeModel.setLocalip(messageReceive.get("localip"));
-        }
-        if (messageReceive.get("fwname") != null) {
-            installedNodeModel.setName(messageReceive.get("fwname"));
-            String mFwName =  messageReceive.get("fwname");
-            Log.d("SaveAndPersist", "Executed by = " +mFwName);
-        }
-        if (messageReceive.get("fwversion") != null) {
-            installedNodeModel.setFwVersion(messageReceive.get("fwversion"));
-        }
-        if (messageReceive.get("online") != null) {
-            installedNodeModel.setOnline(messageReceive.get("online"));
-        }
-        if (messageReceive.get("signal") != null) {
-            installedNodeModel.setSignal(messageReceive.get("signal"));
-        }
-        if (messageReceive.get("uptime") != null) {
-            installedNodeModel.setUptime(messageReceive.get("uptime"));
-        }
-        if (messageReceive.get("reset") != null) {
-            installedNodeModel.setReset(messageReceive.get("reset"));
-        }
-        if (messageReceive.get("ota") != null) {
-            installedNodeModel.setOta(messageReceive.get("ota"));
-        }
-
-        String mNodeid = installedNodeModel.getNodesID();
-        Log.d("SaveAndPersist", "Executed by = " +mNodeid);
-
-        dbNodeRepo.update(installedNodeModel);
-        adapter = new OlmatixAdapter(dbNodeRepo.getNodeList());
-        mRecycleView.setAdapter(adapter);
-        data.clear();
-        data.addAll(dbNodeRepo.getNodeList());
-        messageReceive.clear();
-
-    }
-
-
     @Override
     public void onStart() {
-        Intent i = new Intent(getActivity(), OlmatixService.class);
-        getActivity().startService(i);
 
-        /*LocalBroadcastManager.getInstance(getActivity()).registerReceiver(
-                mMessageReceiver, new IntentFilter("messageMQTT"));*/
+       /* if (flagReceiver==0) {
+            *//*Intent i = new Intent(getActivity(), OlmatixService.class);
+            getActivity().startService(i);*//*
+
+            LocalBroadcastManager.getInstance(getActivity()).registerReceiver(
+                    mMessageReceiver, new IntentFilter("messageMQTT"));
+            Log.d("Receiver ", "Installed_Node = Starting..");
+            flagReceiver = 1;
+        }*/
         super.onStart();
     }
 
     @Override
     public void onPause() {
-        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(mMessageReceiver);
-
+        /*LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(mMessageReceiver);
+*/
         super.onPause();
     }
 
     @Override
     public void onResume() {
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(
-                mMessageReceiver, new IntentFilter("messageMQTT"));
+        adapter = new OlmatixAdapter(dbNodeRepo.getNodeList());
+        mRecycleView.setAdapter(adapter);
         super.onResume();
     }
 
 
 
-        private View.OnClickListener mFabClickListener() {
+    private View.OnClickListener mFabClickListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final EditText mEditText = new EditText(getContext());
+                final boolean addnode = true;
                 new AlertDialog.Builder(getContext())
                         .setTitle("Add Node")
                         .setMessage("Please type Olmatix product ID!")
@@ -291,8 +144,7 @@ public class Installed_Node extends Fragment implements OnStartDragListener {
                                     subToken.setActionCallback(new IMqttActionListener() {
                                         @Override
                                         public void onSuccess(IMqttToken asyncActionToken) {
-                                            messageReceive.put("NodeId",inputResult);
-                                            flagNodeAdd = 1;
+
                                         }
 
                                         @Override

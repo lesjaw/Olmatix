53af070846edc2eed6b12ad00a0b5c13eb82b5b0
Lesjaw <lesjaw@gmail.com>
2016-12-11 23:24:48 +0700

    remove starting service from this

--- olmatix/src/main/java/com/olmatix/ui/activity/SplashActivity.java
+++ olmatix/src/main/java/com/olmatix/ui/activity/SplashActivity.java
@@ -1,16 +1,10 @@
 package com.olmatix.ui.activity;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.olmatix.service.OlmatixService;
 
 /**
  * Created by Lesjaw on 04/12/2016.
@@ -18,24 +12,28 @@ import com.olmatix.service.OlmatixService;
 
 public class SplashActivity extends AppCompatActivity {
 
-
+    int flagReceiver =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         String mServerURL = sharedPref.getString("server_address", "cloud.olmatix.com");
-        Log.d("DEBUG", "Server Address 1: " + mServerURL);
         String mServerPort = sharedPref.getString("server_port", "1883");
-        Log.d("DEBUG", "Server Port 1: " + mServerPort);
         String mUserName = sharedPref.getString("user_name", "olmatix1");
-        Log.d("DEBUG", "User Name 1: " + mUserName);
         String mPassword = sharedPref.getString("password", "olmatix");
+        /*Log.d("DEBUG", "Server Address 1: " + mServerURL);
+        Log.d("DEBUG", "Server Port 1: " + mServerPort);
+        Log.d("DEBUG", "User Name 1: " + mUserName);
         Log.d("DEBUG", "Password 1: " + mPassword);
+*/
 
-
-        Intent i = new Intent(this, OlmatixService.class);
-        startService(i);
+        /*if (flagReceiver==0) {
+            Intent i = new Intent(this, OlmatixService.class);
+            startService(i);
+            flagReceiver =1;
+            Log.d("SplashActivity = ", "Starting OlmatixService");
+        }*/
 
         if (mUserName.equals("olmatix1") ) {
             Intent intent = new Intent(this, SettingsActivity.class);

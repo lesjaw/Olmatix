2dc37d131998e019f285d988e423c673fb5553bb
jolly_raiyani <jolly.mptechnolabs@gmail.com>
2016-12-13 19:41:04 +0530

    detail view

--- olmatix/src/main/java/com/olmatix/adapter/NodeDetailAdapter.java
+++ olmatix/src/main/java/com/olmatix/adapter/NodeDetailAdapter.java
@@ -81,7 +81,7 @@ public class NodeDetailAdapter  extends RecyclerView.Adapter<NodeDetailAdapter.O
             @Override
             public void onClick(View view) {
                 if (Connection.getClient().isConnected()) {
-                    String topic = "devices/"+mInstalledNodeModel.getNode_id()+"/light/0/set";
+                    String topic = "devices/"+mInstalledNodeModel.getNode_id()+"/light/"+mInstalledNodeModel.getChannel()+"/set";
                     String payload = "ON";
                     byte[] encodedPayload = new byte[0];
                     try {
@@ -103,7 +103,7 @@ public class NodeDetailAdapter  extends RecyclerView.Adapter<NodeDetailAdapter.O
             @Override
             public void onClick(View view) {
                 if (Connection.getClient().isConnected()) {
-                    String topic = "devices/"+mInstalledNodeModel.getNode_id()+"/light/0/set";
+                    String topic = "devices/"+mInstalledNodeModel.getNode_id()+"/light/"+mInstalledNodeModel.getChannel()+"/set";
                     String payload = "OFF";
                     byte[] encodedPayload = new byte[0];
                     try {

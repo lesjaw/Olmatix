f69e3e6dc6cfeaca95984fbbfdfa366347cdeffb
Lesjaw <lesjaw@gmail.com>
2017-01-11 15:14:37 +0700

    add setup product page

--- olmatix/src/main/java/com/olmatix/ui/activity/SetupProduct.java
+++ olmatix/src/main/java/com/olmatix/ui/activity/SetupProduct.java
@@ -1,12 +1,22 @@
 package com.olmatix.ui.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.olmatix.lesjaw.olmatix.R;
+
+import java.util.List;
 
 import ernestoyaquello.com.verticalstepperform.VerticalStepperFormLayout;
 import ernestoyaquello.com.verticalstepperform.interfaces.VerticalStepperForm;
@@ -22,6 +32,12 @@ public class SetupProduct extends AppCompatActivity implements VerticalStepperFo
     private static final int CONNECT_TO_PRODUCT = 0;
     private static final int CHOOSE_WIFI = 1;
     private static final int TYPE_WIFI_PASSWORD = 2;
+
+    private WifiManager mainWifi;
+    private WifiReceiver receiverWifi;
+    ListAdapter adapter;
+    ListView listWifiDetails;
+    List wifiList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +99,26 @@ public class SetupProduct extends AppCompatActivity implements VerticalStepperFo
     connectText = new TextView(this);
     connectText.setText("Choose WiFi/SSID Product ");
 
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        receiverWifi = new WifiReceiver();
+        listWifiDetails = new ListView(this);
+
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mainWifi.startScan();
+        wifiList  = mainWifi.getScanResults();
+
+        ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, wifiList);
+
+        listWifiDetails.setAdapter(modeAdapter);
+
+
         return connectText;
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+        }
     }
 
     private View createChooseTitleStep() {
